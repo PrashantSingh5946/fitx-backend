ccb3a61bff8a80b4f90f6c250ef477ff114292a1
Prashant Singh <octanesingh@gmail.com>
2024-03-27 20:14:14 +0530

    adding middleware

---
+++ lib/middlewares/verifyToken.ts
@@ -0,0 +1,22 @@
+import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+
+module.exports = verifyToken;
+
+function verifyToken(req: Request, res: Response, next: NextFunction) {
+  const token = req.header("Authorization");
+  if (!token) return res.status(401).json({ error: "Access denied" });
+
+  try {
+    const decoded: any = jwt.verify(token, process.env.ENCRYPTION_KEY!);
+
+    req.body.email = decoded.email;
+    console.log("🚀 ~ Verified token - ", req.body.email);
+
+    next();
+  } catch (error) {
+    res.status(401).json({ error: "Invalid token" });
+  }
+}
+
+export default verifyToken;
